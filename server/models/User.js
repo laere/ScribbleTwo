a63ab2e1f8482fb44762b5b69ee54ae2b6168f68
Zack Bostian <bostianz@gmail.com>
2019-07-02 02:52:01 -0400

    Added user model boilerplate

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,39 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = {};
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50
+  },
+  profilename: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 24
+  },
+  avatar: {
+    type: String
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 const User = mongoose.model("users", userSchema);
 module.exports = User;
